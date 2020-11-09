dc8895a9afa4cadc7c2de3c89b16f2e7518f5971
vakaluk.oleksandr <vakaluk.oleksandr@gmail.com>
2020-11-09 23:10:09 +0200

    ADDED: repository test

--- src/test/java/com/sashko/taskmongo/NodeRepositoryUnitTest.java
+++ src/test/java/com/sashko/taskmongo/NodeRepositoryUnitTest.java
@@ -1,35 +1,28 @@
 package com.sashko.taskmongo;
 
 
-import java.util.List;
-
 import com.sashko.taskmongo.models.Node;
 import com.sashko.taskmongo.repositories.NodeRepository;
-import com.sashko.taskmongo.services.NodeService;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.BDDMockito.given;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.stereotype.Component;
 
-@ExtendWith(MockitoExtension.class)
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@DataMongoTest(includeFilters = @Filter(Component.class))
 public class NodeRepositoryUnitTest {
 
-    @Mock
-    private NodeRepository nodeRepository;
+    @Autowired
+    NodeRepository nodeRepository;
 
     @Test
     void shouldSaveNodeSuccessfully(){
-        final Node node = new Node("1", "test1", "test1");
-
-        given(nodeRepository.save(node)).willAnswer(invocation->invocation.getArgument(0));
-
-        List<Node> savedNodes = nodeRepository.findAll();
-
-        assertThat(savedNodes.size()).isGreaterThanOrEqualTo(1);
-
+        Node node = new Node("1", "test1", "test1");
+        Node returnedNode = nodeRepository.save(node);
+        assertThat(returnedNode).isNotNull();
     }
 }
